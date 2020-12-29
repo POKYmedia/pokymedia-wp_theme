b5824003cc69d3cbc51dba839148708e04ed7f43
František Gič <mcfreddie777@gmail.com>
2020-12-29 22:52:18 +0100

    Create header and footer

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="referrer" content="no-referrer"/>
+    <meta name="viewport" content="initial-scale=1.0"/>
+
+    <meta name="author" content="František Gič"/>
+    <meta
+            name="description"
+            content="<?php echo get_bloginfo('description') ?>"
+    />
+
+    <!-- Facebook Meta Tags -->
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="<?php echo get_bloginfo('name') ?>"/>
+    <meta property="og:url" content="<?php echo get_bloginfo('url') ?>"/>
+    <meta
+            property="og:description"
+            content="<?php echo get_bloginfo('description') ?>"
+    />
+    <meta property="og:image" content="<?php echo get_site_icon_url() ?>"/>
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:title" content="<?php echo get_bloginfo('name') ?>"/>
+    <meta
+            name="twitter:description"
+            content="<?php echo get_bloginfo('description') ?>"
+    />
+    <meta property="twitter:url" content="<?php echo get_bloginfo('url') ?>"/>
+    <meta name="twitter:image" content="<?php echo get_site_icon_url() ?>"/>
+
+    <link rel="icon" type="image/png" href="<?php echo get_site_icon_url() ?>"/>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
