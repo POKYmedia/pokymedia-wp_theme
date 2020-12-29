06483ce5de4a3ebee04adba7e537a5e5be20430f
František Gič <mcfreddie777@gmail.com>
2020-12-29 22:53:23 +0100

    Add scripts and styles

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+define( 'THEME_DIRECTORY', get_template_directory() );
+define( 'THEME_DIRECTORY_URI', get_template_directory_uri() );
+
+/**
+ * Scripts and styles
+ */
+require_once THEME_DIRECTORY . '/includes/scripts-and-styles.php';
---
+++ includes/scripts-and-styles.php
@@ -0,0 +1,13 @@
+<?php
+
+add_action('wp_enqueue_scripts', function () {
+    // styles
+    wp_enqueue_style('wp-style', get_stylesheet_uri());
+    wp_enqueue_style('poky-vendor-style', THEME_DIRECTORY_URI . '/css/vendor.min.css');
+    wp_enqueue_style('poky-style', THEME_DIRECTORY_URI . '/css/script.min.css');
+
+    // scripts
+    wp_enqueue_script('poky-vendor-script', THEME_DIRECTORY_URI . '/js/vendor.min.js', null, null, true);
+    wp_enqueue_script('poky-script', THEME_DIRECTORY_URI . '/js/script.min.js',null, null, true);
+});
+
