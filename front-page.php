e86bd603678e5d0403b0a6c89f8e116cfb258230
František Gič <mcfreddie777@gmail.com>
2021-01-02 19:38:21 +0100

    Render new event type on front page

--- front-page.php
+++ front-page.php
@@ -2,22 +2,31 @@
 
 <?php get_template_part('partials/quote') ?>
 
-<section id="gallery" class="p-4 md:px-8 md:py-16 lg:px-0">
-    <div class="container mx-auto">
-        <div class="flex flex-col md:flex-row row">
-            <div class="w-full row-item fill">
-                <img src="./img/wedding/wedding-1.jpeg" alt="" />
-            </div>
-            <div class="w-full row-item fill">
-                <img src="./img/wedding/wedding-2.jpeg" alt="" />
-            </div>
-            <div class="w-full row-item fill">
-                <img src="./img/wedding/wedding-3.jpg" alt="" />
-            </div>
-        </div>
-    </div>
-</section>
+<?php if (have_posts()) : ?>
+    <?php global $wp_query; ?>
+    <section id="gallery" class="p-4 md:px-8 md:py-16 lg:px-0">
+        <div class="container mx-auto">
+            <!-- first row start -->
+            <div class="flex flex-col md:flex-row row">
 
+                <?php while (have_posts()) : the_post() ?>
+                    <a href="<?php the_permalink() ?>" class="fill w-full row-item">
+                        <?php the_post_thumbnail(); ?>
+                    </a>
+
+                    <?php
+                    // end row and create new one
+                    $post_order = $wp_query->current_post + 1;
+                    if ($post_order % 3 === 0 && ($post_order !== $wp_query->post_count)) {
+                        echo '</div><div class="flex flex-col md:flex-row row">';
+                    }
+                    ?>
+                <?php endwhile ?>
+            </div>
+            <!-- last row end -->
+        </div>
+    </section>
+<?php endif ?>
 
 <?php get_template_part('partials/testimonials') ?>
 
