fe9f57a33e68e9de3589c364d117fce06c9353a3
František Gič <mcfreddie777@gmail.com>
2021-01-02 15:55:16 +0100

    Create 404 page

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header() ?>
+
+<!-- TODO: full screen height -->
+<div class="flex flex-col w-full bg-beige-100 text-center py-20 text-gray-900">
+    <h1 class="text-6xl">
+        404
+    </h1>
+    <p class="text-xl">
+        <?php _e('Page not found', 'pokymedia') ?>
+    </p>
+</div>
+<?php get_footer() ?>
