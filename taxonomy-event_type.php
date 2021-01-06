06fe0cda4b9c19cdaf0052cb0324990765c9864a
František Gič <mcfreddie777@gmail.com>
2021-01-06 14:59:02 +0100

    Create taxonomy template where all events are listed

--- taxonomy-event_type.php
+++ taxonomy-event_type.php
@@ -1,15 +1,16 @@
 <?php get_header() ?>
-
 <?php get_template_part('partials/quote') ?>
 
-<?php if (have_posts()) : ?>
-    <?php global $wp_query; ?>
-    <section id="gallery" class="p-4 md:px-8 md:py-16 lg:px-0">
-        <div class="container mx-auto">
+<section id="gallery" class="p-4 md:px-8 md:py-16 lg:px-0">
+    <div class="container mx-auto">
+
+        <?php if (have_posts()) : global $wp_query ?>
+
             <!-- first row start -->
             <div class="flex flex-col md:flex-row row">
 
                 <?php while (have_posts()) : the_post() ?>
+
                     <a href="<?php the_permalink() ?>" class="fill w-full row-item">
                         <?php the_post_thumbnail(); ?>
                     </a>
@@ -21,13 +22,20 @@
                         echo '</div><div class="flex flex-col md:flex-row row">';
                     }
                     ?>
+
                 <?php endwhile ?>
+
             </div>
             <!-- last row end -->
-        </div>
-    </section>
-<?php endif ?>
+
+        <?php else: ?>
+            <div class="w-5/6 mx-auto py-10">
+                <p><?php _e("Sorry, this section does not contain any posts yet.", 'pokymedia') ?></p>
+            </div>
+        <?php endif ?>
+
+    </div>
+</section>
 
 <?php get_template_part('partials/testimonials') ?>
-
 <?php get_footer() ?>
