7742e431b4aedb4f6c9c8ecf69cd3c23c0a21a9e
František Gič <mcfreddie777@gmail.com>
2021-03-25 18:53:53 +0100

    Implement simple masonry grid gallery

--- single-event.php
+++ single-event.php
@@ -14,17 +14,16 @@ get_template_part(
 )
 ?>
 
-<section>
-<!--    <p>--><?php //the_content() ?><!--</p>-->
-    <?php $images = get_post_meta(get_the_ID(), 'vdw_gallery_id', true); ?>
-
+<section id="mosaic-gallery" class="masonry container mx-auto py-20 px-10">
     <?php
+    $images = get_post_meta(get_the_ID(), 'vdw_gallery_id', true);
     if (!empty($images)) :
         foreach ($images as $image_id): ?>
-            <img src="<?php echo wp_get_attachment_url($image_id) ?>" alt="">
+            <div class="fill flex-auto">
+                <img class="masonry-brick" src="<?php echo wp_get_attachment_url($image_id) ?>" alt="">
+            </div>
         <?php endforeach;
     endif ?>
-
 </section>
 
 <?php get_footer() ?>
