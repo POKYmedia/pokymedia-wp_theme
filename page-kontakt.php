5f4e648dfee39a6743018e465da02ed3cff5cd7e
František Gič <mcfreddie777@gmail.com>
2021-06-10 21:24:29 +0200

    Edit o nas & kontakt page styles

--- page-kontakt.php
+++ page-kontakt.php
@@ -1,5 +1,9 @@
 <?php get_header() ?>
 <?php get_template_part('partials/header') ?>
 
+<div class="container mx-auto my-10 font-serif">
+    <?php the_content(); ?>
+</div>
+
 <?php get_footer(null, ['hide' => ['contact']]) ?>
 
--- page.php
+++ page.php
@@ -1,4 +1,8 @@
 <?php get_header() ?>
 <?php get_template_part('partials/header') ?>
 
+<div class="container mx-auto my-10 font-serif">
+    <?php the_content(); ?>
+</div>
+
 <?php get_footer() ?>
